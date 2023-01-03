ae4b128469a8eaac8b47a855c46bd87f2f49b624
Peterson Larson <peter-larson@hotmail.com>
2023-01-03 16:11:08 -0300

    feature/homepage_and_tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/header';
+import Home from './components/home';
+import About from './components/about';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
+      <About />
     </div>
   );
 }
